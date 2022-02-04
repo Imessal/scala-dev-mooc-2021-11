2faf6cd606bd28653c7c73153b55a9abdc0e490c
mgorbunov <magorbunov@totsystems.ru>
2022-02-04 18:40:40 +0500

    hw2 minor fixes

--- src/test/scala/collections/check_collections_task.scala
+++ src/test/scala/collections/check_collections_task.scala
@@ -10,9 +10,10 @@ class check_collections_task extends AnyFlatSpec {
     assert(capitalizeIgnoringASCII(List("Оказывается", ",", "ЗвУк", "КЛАВИШЬ")) === List("Оказывается", ",", "звук", "клавишь"))
   }
 
+  // раз в задании написано "числа от 0 до 9", то десяти быть не должно
   "check numbersToNumericString" should "ok" in {
-    val text = "Hello. I am 10 years old"
-    val transformText = "Hello. I am ten years old"
+    val text = "Hello. I am 9 years old"
+    val transformText = "Hello. I am nine years old"
     assert(numbersToNumericString(text) === transformText)
     assert(numbersToNumericString("") === "")
     assert(numbersToNumericString("4") === "four")
