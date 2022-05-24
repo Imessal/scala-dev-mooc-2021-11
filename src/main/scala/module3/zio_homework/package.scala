fdb7d1fc42fa3ed22447275803514ab95263cac7
mgorbunov <magorbunov@totsystems.ru>
2022-05-24 10:35:47 +0500

    hw6 fix tabs

--- src/main/scala/module3/zio_homework/package.scala
+++ src/main/scala/module3/zio_homework/package.scala
@@ -1,171 +1,169 @@
 package module3
 
 import module3.zio_homework.runningTimeService.RunningTimeService
-import pureconfig.{ConfigObjectSource, ConfigSource}
 import zio.clock.Clock
 import zio.console._
 import zio.duration.durationInt
 import zio.random._
-import zio.{ExitCode, IO, Ref, Task, UIO, URIO, ZIO}
+import zio.{ExitCode, IO, Ref, UIO, URIO, ZIO}
 
 import java.io.IOException
-import java.nio.file.Paths
 import scala.language.postfixOps
 import scala.util.Try
 
 package object zio_homework {
-	/**
-	 * 1.
-	 * Используя сервисы Random и Console, напишите консольную ZIO программу которая будет предлагать пользователю угадать число от 1 до 3
-	 * и печатать в когнсоль угадал или нет. Подумайте, на какие наиболее простые эффекты ее можно декомпозировать.
-	 */
+    /**
+     * 1.
+     * Используя сервисы Random и Console, напишите консольную ZIO программу которая будет предлагать пользователю угадать число от 1 до 3
+     * и печатать в когнсоль угадал или нет. Подумайте, на какие наиболее простые эффекты ее можно декомпозировать.
+     */
 
-	private def zParseInt(number: String): IO[Option[Nothing], Int] = {
-		ZIO.fromOption {
-			Try(number.toInt).toOption
-		}
-	}
+    private def zParseInt(number: String): IO[Option[Nothing], Int] = {
+        ZIO.fromOption {
+            Try(number.toInt).toOption
+        }
+    }
 
-	lazy val guess = getStrLn.flatMap(zParseInt).tapError(_ => putStrLn("It's not a number :("))
-	lazy val guessProgram: URIO[Console with Random, ExitCode] = {
-		for {
-			_ 		   <- zio.console.putStrLn("Guess number?")
-			number 	   <- nextIntBounded(3)
-			userNumber <- guess.eventually
+    lazy val guess = getStrLn.flatMap(zParseInt).tapError(_ => putStrLn("It's not a number :("))
+    lazy val guessProgram: URIO[Console with Random, ExitCode] = {
+        for {
+            _          <- zio.console.putStrLn("Guess number?")
+            number     <- nextIntBounded(3)
+            userNumber <- guess.eventually
 
-			_ <- if (number == userNumber) {
-				putStrLn("Right!")
-			} else {
-				putStrLn("Wrong :( Another try?") *> guessProgram
-			}
-		} yield ()
-	}.exitCode
+            _ <- if (number == userNumber) {
+                putStrLn("Right!")
+            } else {
+                putStrLn("Wrong :( Another try?") *> guessProgram
+            }
+        } yield ()
+    }.exitCode
 
-	/**
-	 * 2. реализовать функцию doWhile (общего назначения), которая будет выполнять эффект до тех пор, пока его значение в условии не даст true
-	 *
-	 */
+    /**
+     * 2. реализовать функцию doWhile (общего назначения), которая будет выполнять эффект до тех пор, пока его значение в условии не даст true
+     *
+     */
 
-	def doWhile[R, E, A](effect: ZIO[R, E, A])(p: A => Boolean): ZIO[R, E, A] = {
-		for {
-			result <- effect
-			eff    <- if(p(result)) ZIO.succeed(result) else doWhile(effect)(p)
-		} yield eff
-	}
+    def doWhile[R, E, A](effect: ZIO[R, E, A])(p: A => Boolean): ZIO[R, E, A] = {
+        for {
+            result <- effect
+            eff    <- if (p(result)) ZIO.succeed(result) else doWhile(effect)(p)
+        } yield eff
+    }
 
-	/**
-	 * 3. Реализовать метод, который безопасно прочитает конфиг из файла, а в случае ошибки вернет дефолтный конфиг
-	 * и выведет его в консоль
-	 * Используйте эффект "load" из пакета config
-	 */
+    /**
+     * 3. Реализовать метод, который безопасно прочитает конфиг из файла, а в случае ошибки вернет дефолтный конфиг
+     * и выведет его в консоль
+     * Используйте эффект "load" из пакета config
+     */
 
-	def loadConfigOrDefault: ZIO[Console, Throwable, Unit] = {
-		val defaultConfig = config.AppConfig(appName = "foo", appUrl = "bar")
+    def loadConfigOrDefault: ZIO[Console, Throwable, Unit] = {
+        val defaultConfig = config.AppConfig(appName = "foo", appUrl = "bar")
 
-		config.load.tapError(_ => ZIO.succeed(defaultConfig)).flatMap { conf =>
-			putStrLn(conf.toString)
-		}
-	}
-
-
-	/**
-	 * 4. Следуйте инструкциям ниже для написания 2-х ZIO программ,
-	 * обратите внимание на сигнатуры эффектов, которые будут у вас получаться,
-	 * на изменение этих сигнатур
-	 */
+        config.load.tapError(_ => ZIO.succeed(defaultConfig)).flatMap { conf =>
+            putStrLn(conf.toString)
+        }
+    }
 
 
-	/**
-	 * 4.1 Создайте эффект, который будет возвращать случайеым образом выбранное число от 0 до 10 спустя 1 секунду
-	 * Используйте сервис zio Random
-	 */
-	lazy val eff: ZIO[Random with Clock with Random, Nothing, Int] = {
-		ZIO.sleep(1 second) *> zio.random.nextIntBounded(10)
-	}
-
-	/**
-	 * 4.2 Создайте коллукцию из 10 выше описанных эффектов (eff)
-	 */
-	lazy val effects: Seq[ZIO[Random with Clock with Random, Nothing, Int]] = List.fill(10)(eff)
+    /**
+     * 4. Следуйте инструкциям ниже для написания 2-х ZIO программ,
+     * обратите внимание на сигнатуры эффектов, которые будут у вас получаться,
+     * на изменение этих сигнатур
+     */
 
 
-	/**
-	 * 4.3 Напишите программу которая вычислит сумму элементов коллекци "effects",
-	 * напечатает ее в консоль и вернет результат, а также залогирует затраченное время на выполнение,
-	 * можно использовать ф-цию printEffectRunningTime, которую мы разработали на занятиях
-	 */
+    /**
+     * 4.1 Создайте эффект, который будет возвращать случайеым образом выбранное число от 0 до 10 спустя 1 секунду
+     * Используйте сервис zio Random
+     */
+    lazy val eff: ZIO[Random with Clock with Random, Nothing, Int] = {
+        ZIO.sleep(1 second) *> zio.random.nextIntBounded(10)
+    }
 
-	type appType = ZIO[Random with Clock with Random with Console, IOException, Unit]
-	type innerAppType = ZIO[Random with Clock, Nothing, Int]
-
-	lazy val zEffectsSum: innerAppType = effects.reduce[innerAppType] { case (z1, z2) =>
-		for {
-			v1 <- z1
-			v2 <- z2
-		} yield v1 + v2
-	}
-
-	lazy val app: appType = {
-		for {
-			_ 	   <- putStrLn("Starting...")
-			result <- zEffectsSum // закомментил для последнего задания result <- printEffectRunningTime(zEffectsSum)
-			_      <- putStrLn(s"Result: $result")
-		} yield ()
-	}
+    /**
+     * 4.2 Создайте коллукцию из 10 выше описанных эффектов (eff)
+     */
+    lazy val effects: Seq[ZIO[Random with Clock with Random, Nothing, Int]] = List.fill(10)(eff)
 
 
-	/**
-	 * 4.4 Усовершенствуйте программу 4.3 так, чтобы минимизировать время ее выполнения
-	 */
+    /**
+     * 4.3 Напишите программу которая вычислит сумму элементов коллекци "effects",
+     * напечатает ее в консоль и вернет результат, а также залогирует затраченное время на выполнение,
+     * можно использовать ф-цию printEffectRunningTime, которую мы разработали на занятиях
+     */
 
-	val counter: UIO[Ref[Int]] = Ref.make(0)
-	lazy val zEffectsSumSpeedUp: ZIO[Random with Clock, Nothing, Ref[Int]] = for {
-		ref <- counter
-		_ 	<- ZIO.foreachPar(effects) { eff =>
-			eff.flatMap(value => ref.getAndUpdate(_ + value))
-		}
-	} yield {
-		ref
-	}
+    type appType = ZIO[Random with Clock with Random with Console, IOException, Unit]
+    type innerAppType = ZIO[Random with Clock, Nothing, Int]
 
-	lazy val appSpeedUp: ZIO[Console with Clock with Random, IOException, Unit] = {
-		for {
-			_ 		  <- putStrLn("Starting...")
-			resultRef <- zEffectsSumSpeedUp // закомментил для последнего задания result <- printEffectRunningTime(zEffectsSumSpeedUp)
-			result 	  <- resultRef.get
-			_ 		  <- putStrLn(s"Result: $result")
-		} yield ()
-	}
+    lazy val zEffectsSum: innerAppType = effects.reduce[innerAppType] { case (z1, z2) =>
+        for {
+            v1 <- z1
+            v2 <- z2
+        } yield v1 + v2
+    }
 
-
-	/**
-	 * 5. Оформите ф-цию printEffectRunningTime разработанную на занятиях в отдельный сервис, так чтобы ее
-	 * молжно было использовать аналогично zio.console.putStrLn например
-	 */
+    lazy val app: appType = {
+        for {
+            _      <- putStrLn("Starting...")
+            result <- zEffectsSum // закомментил для последнего задания result <- printEffectRunningTime(zEffectsSum)
+            _      <- putStrLn(s"Result: $result")
+        } yield ()
+    }
 
 
-	/**
-	 * 6.
-	 * Воспользуйтесь написанным сервисом, чтобы созадть эффект, который будет логировать время выполнения прогаммы из пункта 4.3
-	 *
-	 *
-	 */
+    /**
+     * 4.4 Усовершенствуйте программу 4.3 так, чтобы минимизировать время ее выполнения
+     */
 
-	type AppEnv = Random with Clock with Random with Console with RunningTimeService
-	val appWithTimeLogg: ZIO[AppEnv, IOException, Unit] = for {
-		_ <- putStrLn("CONSEQUENT: \n")
-		_ <- RunningTimeService.myPrintEffectRunningTime(app)
-		_ <- putStrLn("\nPARALLEL:")
-		_ <- RunningTimeService.myPrintEffectRunningTime(appSpeedUp)
-	} yield ()
+    val counter: UIO[Ref[Int]] = Ref.make(0)
+    lazy val zEffectsSumSpeedUp: ZIO[Random with Clock, Nothing, Ref[Int]] = for {
+        ref <- counter
+        _ <- ZIO.foreachPar(effects) { eff =>
+            eff.flatMap(value => ref.getAndUpdate(_ + value))
+        }
+    } yield {
+        ref
+    }
 
-	/**
-	 *
-	 * Подготовьте его к запуску и затем запустите воспользовавшись ZioHomeWorkApp
-	 */
+    lazy val appSpeedUp: ZIO[Console with Clock with Random, IOException, Unit] = {
+        for {
+            _         <- putStrLn("Starting...")
+            resultRef <- zEffectsSumSpeedUp // закомментил для последнего задания result <- printEffectRunningTime(zEffectsSumSpeedUp)
+            result    <- resultRef.get
+            _         <- putStrLn(s"Result: $result")
+        } yield ()
+    }
 
-	lazy val runApp: URIO[Clock with Random with Console, ExitCode] = {
-		appWithTimeLogg.provideSomeLayer[Clock with Random with Console](RunningTimeService.live).exitCode
-	}
+
+    /**
+     * 5. Оформите ф-цию printEffectRunningTime разработанную на занятиях в отдельный сервис, так чтобы ее
+     * молжно было использовать аналогично zio.console.putStrLn например
+     */
+
+
+    /**
+     * 6.
+     * Воспользуйтесь написанным сервисом, чтобы созадть эффект, который будет логировать время выполнения прогаммы из пункта 4.3
+     *
+     *
+     */
+
+    type AppEnv = Random with Clock with Random with Console with RunningTimeService
+    val appWithTimeLogg: ZIO[AppEnv, IOException, Unit] = for {
+        _ <- putStrLn("CONSEQUENT: \n")
+        _ <- RunningTimeService.myPrintEffectRunningTime(app)
+        _ <- putStrLn("\nPARALLEL:")
+        _ <- RunningTimeService.myPrintEffectRunningTime(appSpeedUp)
+    } yield ()
+
+    /**
+     *
+     * Подготовьте его к запуску и затем запустите воспользовавшись ZioHomeWorkApp
+     */
+
+    lazy val runApp: URIO[Clock with Random with Console, ExitCode] = {
+        appWithTimeLogg.provideSomeLayer[Clock with Random with Console](RunningTimeService.live).exitCode
+    }
 
 }
--- src/main/scala/module3/zio_homework/runningTimeService.scala
+++ src/main/scala/module3/zio_homework/runningTimeService.scala
@@ -1,36 +1,36 @@
 package module3.zio_homework
 
 import module3.zioConcurrency.currentTime
-import zio.{Has, ULayer, ZIO, ZLayer}
 import zio.clock.Clock
 import zio.console.{Console, putStrLn}
+import zio.{Has, ULayer, ZIO, ZLayer}
 
 object runningTimeService {
-	type RunningTimeService = Has[RunningTimeService.Service]
+    type RunningTimeService = Has[RunningTimeService.Service]
 
-	object RunningTimeService {
+    object RunningTimeService {
 
-		trait Service {
-			def myPrintEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[Console with Clock with R, E, A]
-		}
+        trait Service {
+            def myPrintEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[Console with Clock with R, E, A]
+        }
 
-		class ServiceImpl extends Service {
-			override def myPrintEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[Console with Clock with R, E, A] = {
-				for {
-					start  <- currentTime
-					z 	   <- zio
-					finish <- currentTime
-					_ 	   <- putStrLn(s"Running time: ${finish - start}").orDie
-				} yield z
-			}
-		}
+        class ServiceImpl extends Service {
+            override def myPrintEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[Console with Clock with R, E, A] = {
+                for {
+                    start  <- currentTime
+                    z      <- zio
+                    finish <- currentTime
+                    _      <- putStrLn(s"Running time: ${finish - start}").orDie
+                } yield z
+            }
+        }
 
-		val live: ULayer[Has[Service]] = ZLayer.succeed(new ServiceImpl)
+        val live: ULayer[Has[Service]] = ZLayer.succeed(new ServiceImpl)
 
-		// accessors
-		def myPrintEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[R with RunningTimeService with Clock with Console, E, A] = {
-			ZIO.accessM(_.get myPrintEffectRunningTime zio)
-		}
-	}
+        // accessors
+        def myPrintEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[R with RunningTimeService with Clock with Console, E, A] = {
+            ZIO.accessM(_.get myPrintEffectRunningTime zio)
+        }
+    }
 
 }
